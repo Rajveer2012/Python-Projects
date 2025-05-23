2310b54e7d2651246f230b6406b724fdb2f6d51a
Rajveer2012 <rajveerparmar0505@gmail.com>
2025-05-23 16:40:12 +0530

    Update demo.py

--- demo.py
+++ demo.py
@@ -1,7 +1,35 @@
-name = input("Enter your name: ")
-age = input("Enter your age: ")
-a = name
-b = age
-del (name,age)
-print("Welcome " + a , sep= "\"\"")
-print("Your Age is " + b)
+# name = input("Enter your name: ")
+# age = input("Enter your age: ")
+# a = name
+# b = age
+# del (name,age)
+# print("Welcome " + a , sep= "\"\"") 
+# print("Your Age is " + b)
+
+
+
+
+
+# while True:
+#     print('\n')
+    
+    
+#     n = int(input("ENTER THE VALUE OF N: "))
+#     res = int(n*(n+1)/2) 
+#     print("The sum of first",n," natural numbers is: ", res)
+
+
+
+
+
+s = "gfg"
+print("G" in s)
+
+
+
+
+
+
+
+
+
